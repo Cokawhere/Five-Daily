39de940bc63562d6b34be747d035bdb8df944575
rana_inthemood <ro7alimansour@gmail.com>
2025-12-01 02:57:25 +0200

    add device preview support and screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/app_router.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -9,21 +11,26 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+    print("Width: ${MediaQuery.of(context).size.width}");
+    print("Height: ${MediaQuery.of(context).size.height}");
 
-      routerConfig: appRouter,
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          builder: DevicePreview.appBuilder,
+          routerConfig: appRouter,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
