c8b0d58bba975e71c064ca8e5ca2879de5de16a2
rana_inthemood <ro7alimansour@gmail.com>
2025-12-01 02:56:40 +0200

    add color constants and layout extension for responsive design

---
+++ lib/constents/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Main Colors
+  static const Color bgLightGrey = Color.fromRGBO(245, 245, 247, 1); // #F5F5F7
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);       // #FFFFFF
+  static const Color textDark = Color.fromRGBO(46, 46, 46, 1);       // #2E2E2E
+
+  // Accent Colors
+  static const Color orange = Color.fromRGBO(255, 169, 77, 1);       // #FFA94D
+  static const Color purple = Color.fromRGBO(177, 151, 252, 1);      // #B197FC
+  static const Color mint = Color.fromRGBO(131, 197, 190, 1);        // #83C5BE
+}
